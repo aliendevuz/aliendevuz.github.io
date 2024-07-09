dd4e9fdcfcf5fda44126d265baac1db5c70c5355
Alien Dev <aliendevuz@gmail.com>
2024-07-09 12:02:01 +0500

    Updated

---
+++ Handle/service-worker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'pwa-sample-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/app.js'
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
